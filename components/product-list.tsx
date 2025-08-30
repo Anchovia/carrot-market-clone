f31bed78b2e836221946cc582e5cd0a487e958c2
Anchovia <hodolye@naver.com>
2025-08-30 21:51:54 +0900

    infinit scrolling

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { getMoreProducts } from "@/app/(tabs)/products/actions";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ListProduct from "./list-product";
 
 interface productListProps {
@@ -21,33 +21,56 @@ export default function ProductList({ initialProducts }: productListProps) {
     const [page, setPage] = useState(0);
     const [isLastPage, setIsLastPage] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const onLoadMoreClick = async () => {
-        setIsLoading(true);
-        const newProducts = await getMoreProducts(page + 1);
-        if (newProducts.length !== 0) {
-            setPage((prev) => prev + 1);
-            setProducts((prev) => [...prev, ...newProducts]); // ... 사용 : [[prev], [newProducts]] -> [prev, newProducts] 각 array의 element들을 풀어줌(js)
-        } else {
-            setIsLastPage(true);
+
+    const trigger = useRef<HTMLSpanElement>(null);
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            async (
+                entries: IntersectionObserverEntry[],
+                observer: IntersectionObserver
+            ) => {
+                const element = entries[0];
+                if (element.isIntersecting && trigger.current) {
+                    observer.unobserve(trigger.current);
+
+                    setIsLoading(true);
+                    const newProducts = await getMoreProducts(page + 1);
+                    if (newProducts.length !== 0) {
+                        setProducts((prev) => [...prev, ...newProducts]); // ... 사용 : [[prev], [newProducts]] -> [prev, newProducts] 각 array의 element들을 풀어줌(js)
+                        setPage((prev) => prev + 1);
+                    } else {
+                        setIsLastPage(true);
+                    }
+                    setIsLoading(false);
+                }
+            },
+            {
+                threshold: 1.0,
+                rootMargin: "0px 0px -100px 0px",
+            }
+        );
+        if (trigger.current) {
+            observer.observe(trigger.current);
         }
 
-        setIsLoading(false);
-    };
+        return () => {
+            observer.disconnect();
+        };
+    }, [page]);
 
     return (
         <div className="p-5 flex flex-col gap-5">
             {products.map((product) => (
                 <ListProduct key={product.id} {...product} />
             ))}
-            {isLastPage ? null : (
-                <button
-                    onClick={onLoadMoreClick}
-                    disabled={isLoading}
+            {!isLastPage ? (
+                <span
+                    ref={trigger}
                     className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
                 >
                     {isLoading ? "로딩 중..." : "더 가져오기"}
-                </button>
-            )}
+                </span>
+            ) : null}
         </div>
     );
 }
