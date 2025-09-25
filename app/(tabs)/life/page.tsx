60dbb6a472e21f6d538fee72dcccc1a1bb1697af
Anchovia <hodolye@naver.com>
2025-09-26 02:35:56 +0900

    cached like

--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,7 +1,65 @@
-export default function Life() {
+import db from "@/lib/db";
+import formatToTimeAgo from "@/lib/utils";
+import Link from "next/link";
+
+async function getPost() {
+    const posts = await db.post.findMany({
+        select: {
+            id: true,
+            title: true,
+            description: true,
+            views: true,
+            created_at: true,
+            _count: {
+                select: {
+                    comments: true,
+                    likes: true,
+                },
+            },
+        },
+    });
+    return posts;
+}
+
+export const metadata = {
+    title: "동네생활",
+};
+
+export default async function Life() {
+    const posts = await getPost();
     return (
-        <div>
-            <h1 className="text-white text-4xl">Life</h1>
+        <div className="p-5 flex flex-col">
+            {posts.map((post) => (
+                <Link
+                    key={post.id}
+                    href={`/posts/${post.id}`}
+                    className="pb-5 mb-5 border-b border-neutral-500 text-neutral-400 flex flex-col last:pb-0 last:border-b-0 gap-2"
+                >
+                    <h2 className="text-white text-lg font-semibold">
+                        {post.title}
+                    </h2>
+                    <p>{post.description}</p>
+                    <div className="flex items-center justify-between text-sm">
+                        <div className="flex gap-4 items-center">
+                            <span>
+                                {formatToTimeAgo(post.created_at.toString())}
+                            </span>
+                            <span>.</span>
+                            <span>조회 {post.views}</span>
+                        </div>
+                        <div className="flex gap-4 items-center *:flex *:gap-1 *:items-center">
+                            <span>
+                                <div className="bg-neutral-500 rounded-full size-4" />
+                                {post._count.likes}
+                            </span>
+                            <span>
+                                <div className="bg-neutral-500 rounded-full size-4" />
+                                {post._count.comments}
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+            ))}
         </div>
     );
 }
---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,178 @@
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import formatToTimeAgo from "@/lib/utils";
+import { revalidateTag, unstable_cache } from "next/cache";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+async function getPost(id: number) {
+    try {
+        const post = db.post.update({
+            where: {
+                id,
+            },
+            // update는 data가 필수
+            data: {
+                views: {
+                    // 1씩 증가시키기
+                    increment: 1,
+                },
+            },
+            include: {
+                user: {
+                    select: {
+                        username: true,
+                        avatar: true,
+                    },
+                },
+                _count: {
+                    select: {
+                        comments: true,
+                    },
+                },
+            },
+        });
+        return post;
+    } catch (e) {
+        return null;
+    }
+}
+
+const getCachedPost = unstable_cache(getPost, ["post-detail"], {
+    tags: [`post-detail`],
+    revalidate: 60,
+});
+
+async function getLikeStatus(postId: number, userId: number) {
+    const isLiked = await db.like.findUnique({
+        where: {
+            id: {
+                postId,
+                userId,
+            },
+        },
+    });
+    const likeCount = await db.like.count({
+        where: {
+            postId,
+        },
+    });
+    return {
+        likeCount,
+        isLike: Boolean(isLiked),
+    };
+}
+
+function getCachedLikeStatus(postId: number, userId: number) {
+    const cachedOperation = unstable_cache(
+        getLikeStatus,
+        ["product-like-status"],
+        {
+            tags: [`like-status-${postId}`],
+        }
+    );
+    return cachedOperation(postId, userId);
+}
+
+export default async function PostDetail({
+    params,
+}: {
+    params: { id: string };
+}) {
+    const id = Number(params.id);
+    if (isNaN(id)) {
+        return notFound();
+    }
+    const post = await getCachedPost(id);
+    if (!post) {
+        return notFound();
+    }
+    const likePost = async () => {
+        "use server";
+        const session = await getSession();
+        try {
+            await db.like.create({
+                data: {
+                    postId: id,
+                    userId: session.id!,
+                },
+            });
+            revalidateTag(`like-status-${id}`);
+        } catch (e) {}
+    };
+    const dislikePost = async () => {
+        "use server";
+        const session = await getSession();
+        try {
+            await db.like.delete({
+                where: {
+                    id: {
+                        postId: id,
+                        userId: session.id!,
+                    },
+                },
+            });
+            revalidateTag(`like-status-${id}`);
+        } catch (e) {
+            console.error("Dislike post error:", e);
+        }
+    };
+    const session = await getSession();
+
+    if (!session.id) {
+        return notFound(); // 또는 로그인 페이지로 리다이렉트
+    }
+
+    const { likeCount, isLike } = await getCachedLikeStatus(id, session.id);
+    return (
+        <div className="p-5 text-white">
+            <div className="flex items-center gap-2 mb-2">
+                <Image
+                    width={28}
+                    height={28}
+                    className="size-7 rounded-full"
+                    src={post.user.avatar ? post.user.avatar : ""}
+                    alt={post.user.username}
+                />
+                <div>
+                    <span className="text-sm font-semibold">
+                        {post.user.username}
+                    </span>
+                    <div className="text-xs">
+                        <span>
+                            {formatToTimeAgo(post.created_at.toString())}
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <h2 className="text-lg font-semibold">{post.title}</h2>
+            <p className="mb-5">{post.description}</p>
+            <div className="flex flex-col gap-5 items-start">
+                <div className="flex items-center gap-2 text-neutral-400 text-sm">
+                    <div className="size-5 bg-neutral-500 rounded-full" />
+                    <span>조회 {post.views}</span>
+                </div>
+                <form action={isLike ? dislikePost : likePost}>
+                    <button
+                        className={`flex items-center gap-2  text-sm border border-neutral-400 rounded-full p-2   transition-colors ${
+                            isLike
+                                ? "bg-orange-500 text-white border-orange-500"
+                                : "text-neutral-400 hover:bg-neutral-800"
+                        }`}
+                    >
+                        {isLike ? (
+                            <div className="size-5 rounded-full bg-neutral-100" />
+                        ) : (
+                            <div className="size-5 rounded-full bg-neutral-500" />
+                        )}
+                        {isLike ? (
+                            <span>{likeCount}</span>
+                        ) : (
+                            <span>공감하기 ({likeCount})</span>
+                        )}
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
